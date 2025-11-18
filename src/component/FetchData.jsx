97dfe3f97d9f2b298b631d3d06bb091f021c0934
david <davidojo.dev@gmail.com>
2025-11-18 21:19:32 +0100

    Update color toggle logic and add button
    Initial color state is now true, and the color logic for the div has been reversed. Added a button to toggle the color state and applied additional styling to the div and button.

--- src/component/FetchData.jsx
+++ src/component/FetchData.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 
 const FetchData = () => {false
 
-    const [changeColor, setChageColor] = useState(false)
+    const [changeColor, setChageColor] = useState(true)
   return (
     <div className='w-full p-10 my-10 px-8 '>
-        <div className={`${changeColor ? "bg-red-600" : "bg-green-400"} size-[300px] `}></div>
+        <div className={`${changeColor ? "bg-green-400" : "bg-red-600"} size-[300px] rounded-2xl`}></div>
+          <button className='border rounded-lg bg-black text-white px-3 py-2 cursor-pointer ' onClick={() => setChageColor((prev)=> !prev)}>Decrement</button>
     </div>
   )
 }
